9113795bf8dbffa9815fc168eda70ed382033a1a
tayoshofunlayo <tayoshofunlayo@gmail.com>
2025-08-30 07:36:18 +0000

    created urls route in my core

---
+++ mysite/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('posts/<int:post_id>/comments/', views.CommentListView.as_view(), name='comment-list'),
+    path('comments/<int:pk>/', views.CommentDetailView.as_view(), name='comment-detail'),
+    path('posts/<int:pk>/like/', views.like_post, name='like-post'),
+]
