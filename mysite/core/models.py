940889a2c33119373e46de6decb439d664a4e0a7
tayoshofunlayo <tayoshofunlayo@gmail.com>
2025-08-30 00:45:08 +0000

    i added comprehensive serializers for blog models including User, Category, Comment, Like and Post with nested relationships and computed fields

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post, Category, Comment, Like
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_date', 'updated_date', 'is_active']
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Like
+        fields = ['id', 'post', 'user', 'created_date']
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author', 'category', 'published_date', 
+                 'updated_date', 'is_published', 'comments', 'likes', 'likes_count', 'comments_count']
+    
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'category', 'is_published']
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['content']
