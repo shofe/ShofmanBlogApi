411bffb34c7c848101dc594a930ad294f7fba336
tayoshofunlayo <tayoshofunlayo@gmail.com>
2025-08-30 07:10:13 +0000

    Registered the modelss in admin.

--- mysite/core/admin.py
+++ mysite/core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Post, Comment, Like
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'published_date', 'is_published']
+    list_filter = ['category', 'published_date', 'is_published']
+    search_fields = ['title', 'content']
+    date_hierarchy = 'published_date'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'created_date', 'is_active']
+    list_filter = ['is_active', 'created_date']
+    search_fields = ['content']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['post', 'user', 'created_date']
+    list_filter = ['created_date']
