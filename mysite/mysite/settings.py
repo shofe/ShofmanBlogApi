f7811bd4ebb896b8631e55fb0b8a29c6fd8cc350
tayoshofunlayo <tayoshofunlayo@gmail.com>
2025-08-31 16:53:51 +0000

    feat: Add Django REST Framework and CORS headers

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -3,11 +3,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-^=6-_k)oh!n9-fpcd1qd0rf(!8y2!!8cc*so1if(!*ydv@*_dc'
+SECRET_KEY = 'your-secret-key-change-this-in-production'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['shofman.mysql.pythonanywhere-services.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    '8000-firebase-blogapi-1756504475594.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -57,16 +61,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # SQLite Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'shofman',
-        'PASSWORD': 'tayosho@21',
-        'HOST': 'shofman.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'use_unicode': True,
-        }
     }
 }
 
@@ -85,25 +81,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://8000-firebase-blogapi-1756504475594.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
-    'https://*.cloudworkstations.dev',
-    'https://*.idx.dev',
-]
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-#CSRF_COOKIE_SAMESITE = 'None'
-#ESSION_COOKIE_SAMESITE = 'None'
-CSRF_TRUSTED_ORIGINS = ['https://shofman.mysql.pythonanywhere-services.com']
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -121,9 +105,12 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "https://8000-firebase-blogapi-1756504475594.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev",
-]
-
 CORS_ALLOW_ALL_ORIGINS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-firebase-blogapi-1756504475594.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
+]
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
